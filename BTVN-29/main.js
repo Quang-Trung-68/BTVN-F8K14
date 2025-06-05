fcc61932e63045595fca6232b156ca2955ad79e4
Quang-Trung-68 <trungsu23@gmail.com>
2025-06-06 01:38:29 +0700

    Add BTVN-29 as a regular folder

---
+++ BTVN-29/main.js
@@ -0,0 +1,42 @@
+const getProducts = async () => {
+  const responds = await fetch("https://fakestoreapi.com/products/");
+  const products = await responds.json();
+  return products;
+};
+
+getProducts().then((products) => {
+  const list = document.querySelector(".card-list");
+  products.forEach((product) => {
+    // create item
+    const item = document.createElement("div");
+    item.setAttribute("class", "card-item");
+    // create thumb
+    const thumb = document.createElement("img");
+    thumb.setAttribute("class", "card-thumb");
+    thumb.setAttribute("src", `${product.image}`);
+    // create title
+    const title = document.createElement("p");
+    title.setAttribute("class", "card-title");
+    title.innerText = product.title;
+    // create price
+    const price = document.createElement("p");
+    price.setAttribute("class", "card-price");
+    price.innerText = "$" + product.price;
+    // create description
+    const description = document.createElement("p");
+    description.setAttribute("class", "card-description");
+    description.innerText = product.description;
+    // create category
+    const category = document.createElement("p");
+    category.setAttribute("class", "card-category");
+    category.innerText = "Category: " + product.category;
+    // create rating
+    const rating = document.createElement("div");
+    rating.setAttribute("class", "card-rating");
+    rating.innerText = `Rating: ⭐ ${product.rating["rate"]} (${product.rating["count"]} reviews)`;
+    // add all to item
+    item.append(thumb, title, price, description, category, rating);
+    // add item to list
+    list.append(item);
+  });
+});

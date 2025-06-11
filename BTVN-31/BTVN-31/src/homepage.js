06c624f0942071127309feb74719a7a5e7c39efe
Quang-Trung-68 <trungsu23@gmail.com>
2025-06-11 10:17:43 +0700

    done v1.1

--- BTVN-31/BTVN-31/src/homepage.js
+++ BTVN-31/BTVN-31/src/homepage.js
@@ -1,19 +1,24 @@
 import { getPostMethod, postGetNewTokenMethod } from "./utils/api.js";
+
 // assign token from local storage
 let token = {
   access: localStorage["access_token"],
   refresh: localStorage["refresh_token"],
 };
+
+
 // function keep access by keep valid token
-const autoKeepAccess = (token) => {
-  setInterval(async () => {
+const homePage = async (token) => {
+  // function auto checking token is not expired or expired, after post to get new access token
     const posts = await getPostMethod(token);
+    // if token access is expired
     if (posts.detail === "token expired") {
       // log post
       console.log(posts);
       const body = {
         refresh: localStorage["refresh_token"],
       };
+      // refresh a new access token
       const tokenNew = await postGetNewTokenMethod(body);
       console.log("New token is:");
       console.log(tokenNew);
@@ -29,20 +34,15 @@ const autoKeepAccess = (token) => {
       console.log("Login token is valid");
       console.log(posts);
     }
-  }, 5000);
 };
-// function to call render homepage
-const homePage = async (token) => {
-  const posts = await getPostMethod(token);
-  console.log(posts);
-  autoKeepAccess(token);
-};
+
 
 homePage(token);
 
 // logout
 const logout = document.querySelector(".logout");
 if (logout) {
+  // go to login page and clear token is saved at localstorage
   logout.onclick = () => {
     document.location.href = "./index.html";
     localStorage.removeItem("access_token");
